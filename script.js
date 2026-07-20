c9b4c9aeb9a7dab70c5eb2f1a58b2fdec74025d7
ATHARV ANAND <atharvanand37@gmail.com>
2026-07-20 16:44:14 +0530

    started with functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+const passwordlength = document.getElementById("passwordlen")
+const submit = document.getElementsByClassName("submit")[0]
+const includelower = document.getElementById("lower")
+const includeupper = document.getElementById("Upper")
+const output = document.getElementById("ps");
+const includenumber = document.getElementById("number")
+
+const includesymbol = document.getElementById("symbol")
+const copy = document.getElementById("cp") ;
